bb7da12d6a7cbc9995e5b5d597115de0286e4d85
DEVASHISH7777 <63115818+DEVASHISH7777@users.noreply.github.com>
2020-05-09 19:09:11 +0200

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -12,6 +12,51 @@ function chargement(){
 
 
 
+function enregistrePseudo(formulaire) {
+
+    let pseudo =  document.getElementById("pseudo").value
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET','enregistrer_nom?usr=' + pseudo,true);
+    xhr.send();
+    return false;
+
+}
+
+
+function traiterPseudo(){
+
+    let pseudo =  document.getElementById("pseudo").value
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET','check_pseudo?usr=' + pseudo ,true);
+    xhr.onload = verifePseudo;
+    xhr.send();
+
+    return false;
+
+}
+
+function verifePseudo(){
+
+    let pseudo =  document.getElementById("pseudo").value
+
+    let reponse = this.response;
+
+    console.log(reponse)
+    if( reponse == 1 ){
+        alert("Ce pseudo existe deja , vous etes bien  : " + pseudo + " Si non trouvé un autre pseudo")
+
+    }
+
+    if( reponse == 0 ){
+        enregistrePseudo();
+        alert("vous avez bien été enregistré vous pouvez passer à la page suivante");
+    }
+    return false;
+}
+
+
 
 
 
